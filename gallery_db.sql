aeb122acd90e20d01d8f4b5b8b6dde805920c6ac
Eman Mohamed <eman.mohamed.ka@gmail.com>
2020-05-27 04:07:02 +0200

    sql upload

--- gallery_db.sql
+++ gallery_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 18, 2020 at 11:29 AM
+-- Generation Time: May 27, 2020 at 04:06 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.1.33
 
@@ -25,6 +25,53 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `photo_id` int(11) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `body` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `photo_id`, `author`, `body`) VALUES
+(5, 3, 'EMAN MOHAMED', 'PHP DEVELOPER');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photos`
+--
+
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `caption` varchar(255) DEFAULT NULL,
+  `description` text NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `alternate_text` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `size` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `photos`
+--
+
+INSERT INTO `photos` (`id`, `title`, `caption`, `description`, `filename`, `alternate_text`, `type`, `size`) VALUES
+(3, 'Koky', 'beautiful girl', 'desc', '1.jpg', 'text', 'jpeg', 5604),
+(4, 'Eman', 'cleaver girl', '', 'Eman.jpg', 'text', 'jpeg', 67166),
+(5, 'Mohamed', NULL, '', 'Mohamed.jpg', '', 'jpeg', 56932),
+(6, 'Morning', NULL, '', 'morning.jpg', '', 'jpeg', 9986);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,12 +80,38 @@ CREATE TABLE `users` (
   `username` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL,
   `first_name` varchar(250) NOT NULL,
-  `last_name` varchar(250) NOT NULL
+  `last_name` varchar(250) NOT NULL,
+  `user_image` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `user_image`) VALUES
+(3, 'WHATEVER', 'pass', 'first_name', 'last_name', NULL),
+(4, 'WHATEVER', '', '', '', NULL),
+(5, 'test', 'test1', 'test2', 'test3', NULL),
+(8, 'emahmoud', '123123', 'Eman', 'Mohamed', NULL),
+(9, 'emahmoud', '123123', 'Eman', 'Mohamed', NULL),
+(10, 'WHATEVER', 'pass', 'first_name', 'last_name', NULL),
+(11, 'WHATEVER', 'pass', 'first_name', 'last_name', NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `photos`
+--
+ALTER TABLE `photos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -51,10 +124,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `photos`
+--
+ALTER TABLE `photos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
