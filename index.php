4444c33920c9f748251b15f477aae8253473105e
emaanmohamed <eman.mohamed.ka@gmail.com>
2020-09-02 13:57:08 +0200

    189

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
 <?php
 
 $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
-$items_per_page = 4;
+$items_per_page = 3;
 
 $items_total_count = Photo::count_all();
 
-//$photos = Photo::find_all();
 $paginate = new Paginate($page, $items_per_page, $items_total_count);
 $sql = "SELECT * FROM photos ";
 $sql .= "LIMIT {$items_per_page} ";
@@ -40,6 +39,15 @@ $photos = Photo::find_by_query($sql);
                 if ($paginate->has_next()) {
                   echo  "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
 
+                }
+                for ($i=1; $i <= $paginate->page_total() ; $i++) {
+                    if ($i == $paginate->current_page) {
+                        echo "<li class='active'><a href='index.php?page={$i}'>{$i}</a></li>";
+
+                    } else {
+	                    echo "<li class='active'><a href='index.php?page={$i}'>{$i}</a></li>";
+
+                    }
                 }
                 if ($paginate->has_previous()) {
                     echo  "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
