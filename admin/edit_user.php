d7eb0d77e0be539c916cc833106c910bd9e59ef5
emaanmohamed <eman.mohamed.ka@gmail.com>
2020-09-11 13:32:33 +0200

    199

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -98,7 +98,7 @@ if (isset($_POST['update'])) {
                             </div>
 
                             <div class="form-group">
-                                <a class="btn btn-danger" href="delete_user.php?id=<?php echo $user->id;?>">Delete</a>
+                                <a id="user-id" class="btn btn-danger" href="delete_user.php?id=<?php echo $user->id;?>">Delete</a>
                                 <input type="submit" name="update" class="btn btn-primary pull-right" value="update" >
                             </div>
 
---
+++ admin/includes/ajax_code.php
@@ -0,0 +1,10 @@
+<?php
+	require ("init.php");
+	if (isset($_POST['image_name'])) {
+		echo "yes";
+	}
+	
+	
+	
+	
+	?>
